5f7587afbafafa7210a3fd4b8f5b758659b06e66
Luke <35667@ma-web.nl>
2023-03-07 15:43:23 +0100

    vertaling toegevoegd

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
-new Date().toLocaleDateString()
-= "9/13/2015"
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("js--clock").innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(currentTime) {
+    if (currentTime < 10) {currentTime = "0" + currentTime};
+    return currentTime;
+  }
