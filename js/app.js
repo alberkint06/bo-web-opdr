05487e397ead7d46eb7b7f5565db9ed91e711f6a
Luke <35667@ma-web.nl>
2023-03-20 14:56:53 +0100

    weather api toegevoegd maar moet nog kijken hoe deze werkt

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const weather = "https://api.open-meteo.com/v1/forecast?latitude=52.37&longitude=4.89&hourly=temperature_2m"
+
 function startTime() {
     const today = new Date();
     let h = today.getHours();
@@ -12,4 +14,21 @@ function startTime() {
   function checkTime(currentTime) {
     if (currentTime < 10) {currentTime = "0" + currentTime};
     return currentTime;
+  }
+
+  function newFunction() {
+      <><script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script><canvas id="myChart" style="width:100%;max-width:700px"></canvas></>;
+      var xValues = ["Water", "Gas", "Light", "electricity"];
+      var yValues = [28, 23, 27, 22];
+      var barColors = ["blue", "grey", "yellow", "green"];
+      var myChart = new myChart("myChart", {
+          type: "bar",
+          data: {
+              labels: xValues,
+              datasets: [{
+                  backgroundColor: barColors,
+                  data: yValues
+              }]
+          },
+      });
   }
